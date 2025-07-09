3767ff66313bddcfe9049b880fd190c4bd57df53
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-07-09 16:25:11 +0100

    implement Requirements methods

--- frontend/script.js
+++ frontend/script.js
@@ -1,25 +1,24 @@
 async function sendMessage() {
-    const text = document.getElementById("message").value;
-    await fetch("http://localhost:8000/message", {
-      method: "POST",
-      headers: { "Content-Type": "text/plain" },
-      body: text
-    });
-    document.getElementById("message").value = "";
-    loadMessages();
-  }
-  
-  async function loadMessages() {
-    const res = await fetch("http://localhost:8000/messages");
-    const data = await res.json();
-    const list = document.getElementById("messages");
-    list.innerHTML = "";
-    data.forEach(msg => {
-      const li = document.createElement("li");
-      li.textContent = msg;
-      list.appendChild(li);
-    });
-  }
-  
-  window.onload = loadMessages;
-  
+  const text = document.getElementById("message").value;
+  await fetch("http://127.0.0.1:8000/message", {
+    method: "POST",
+    headers: { "Content-Type": "text/plain" },
+    body: text
+  });
+  document.getElementById("message").value = "";
+  loadMessages();
+}
+
+async function loadMessages() {
+  const res = await fetch("http://127.0.0.1:8000/messages");
+  const data = await res.json();
+  const list = document.getElementById("messages");
+  list.innerHTML = "";
+  data.forEach(msg => {
+    const li = document.createElement("li");
+    li.textContent = msg;
+    list.appendChild(li);
+  });
+}
+
+window.onload = loadMessages;
